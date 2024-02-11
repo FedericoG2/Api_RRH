2be770515514f9ede10d3a83ba2bf7531e334afe
FedericoG2 <fg0170700@gmail.com>
2024-02-11 19:55:43 -0300

    DTO Estadisticas

---
+++ src/main/java/com/rrh/api/dto/PercentageDTO.java
@@ -0,0 +1,12 @@
+package com.rrh.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PercentageDTO {
+    private double percentage;
+}
