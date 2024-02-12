86d8f52d4db60d7dfd4ea32aa42e2f2988c4a4d0
FedericoG2 <fg0170700@gmail.com>
2024-02-12 14:04:17 -0300

    Metodo para calcular todos los porcentajes

--- src/main/java/com/rrh/api/service/JobVacancyService.java
+++ src/main/java/com/rrh/api/service/JobVacancyService.java
@@ -44,12 +44,36 @@ public class JobVacancyService implements IJobVacancyService {
     @Override
     public PercentageDTO calculatePercentage(Long id) {
         Job_vacancy vacancy = this.findVacancy(id);
+        if (vacancy == null) {
+            throw new IllegalArgumentException("Vacante con id " + id + " no encontrada.");
+        }
+
         int applicants = vacancy.getNumber_applicants();
+        if (applicants == 0) {
+            return new PercentageDTO(0.0, 0.0,0.0, 0.0,0.0, 0.0); // No hay solicitantes, por lo tanto, el porcentaje de entrevistados y exitosos es cero.
+        }
+
         int interviewed = vacancy.getInterviewed();
-        double percentage = ((double) interviewed / applicants) * 100;
-        percentage = Math.round(percentage * 100.0) / 100.0;
-        return new PercentageDTO(percentage);
+        int successful = vacancy.getSuccessful_interviewees();
+        int latamApplicants = vacancy.getLatam_applicants();
+        int usaApplicants = vacancy.getUsa_applicants();
+        int europeApplicants = vacancy.getEurope_applicants();
+        int asiaApplicants = vacancy.getAsia_applicants();
+
+        double percentageInterviewed = calculatePercentage(interviewed, applicants);
+        double percentageSuccessful = calculatePercentage(successful, applicants);
+        double percentageLatam = calculatePercentage(latamApplicants,applicants);
+        double percentageUsa = calculatePercentage(usaApplicants,applicants);
+        double percentageEurope = calculatePercentage(europeApplicants,applicants);
+        double percentageAsia = calculatePercentage(asiaApplicants,applicants);
+
+        return new PercentageDTO(percentageInterviewed, percentageSuccessful,percentageLatam,percentageUsa,percentageEurope,percentageAsia);
     }
+
+    private double calculatePercentage(int value, int total) {
+        return ((double) value / total) * 100;
+    }
+
 
 
 
